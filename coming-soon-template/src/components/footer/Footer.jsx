ad1803c51967114f7d237d2497b4264441d2c0d5
Emin <emin.altan@hotmail.com>
2023-09-13 13:37:35 +0300

    CST-04 Please add background color for <MenuNav/> component Fixes #6

--- coming-soon-template/src/components/footer/Footer.jsx
+++ coming-soon-template/src/components/footer/Footer.jsx
@@ -2,15 +2,17 @@ import "./_footer.scss";
 
 const Footer = () => {
   return (
-    <footer>
-      <img
-        src="https://www.w3schools.com/w3images/map.jpg"
-        alt=""
-      />
+    <>
+      <footer>
+        <img
+          src="https://www.w3schools.com/w3images/map.jpg"
+          alt=""
+        />
+      </footer>
       <p>
         Powered by Emin Altan <a href="">r.com</a>
       </p>
-    </footer>
+    </>
   );
 };
 export default Footer;
